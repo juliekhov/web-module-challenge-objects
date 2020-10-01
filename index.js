2c3db95b6a7943343d6ecc18aa57dc0a225fdbe0
Julie Khov <jkhov002@ucr.edu>
2020-09-30 18:55:46 -0700

    completed objects tasks

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 ///////////////Menu Items (MVP)///////////////////
 
-const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
-const burger = {name: "Burger", price: 18, category: "Lunch"};
-const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
+const latte = { name: "Cafe Latte", price: 4, category: "Drinks" };
+const burger = { name: "Burger", price: 18, category: "Lunch" };
+const breakfastBurrito = { name: "Breakfast Burrito", price: 16, category: "Breakfast" };
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
-function createMenuItem(name, price, category){
-    /* Code here */
+// function with three parameters
+function createMenuItem(itemName, itemPrice, itemCategory) {
+    // declare an object with const
+    const anotherItem = {
+        name: itemName,
+        price: itemPrice,
+        category: itemCategory
+    };
+    // return the object
+    return anotherItem;
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+// call / invoke the function 3 times, each with different arguments.
+// assign the object to a variable name I choose.
+
+const miniTacos = createMenuItem("minitacos", 3, "appetizer");
+const salad = createMenuItem("salad", 6, "appetizer");
+const wings = createMenuItem("wings", 9, "dinner");
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -24,27 +38,40 @@ and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
+// using the object dot notation to create a method (a function within the object)
+// pass through the string as a parameter
+burger.discountPrice(clientType) {
+    // Simple conditional statement can be put into a ternary operator
+    // do i need to use the "this" keyword because i'm accessing price inside object while in burger method?  
+    clientType == "teacher" || clientType == "student" ? price = 0.75 * this.price : price = 0.9 * this.price;
+    // return the value stored in price
+    return price;
+}
 
 
 ///////////////Reviews (MVP)///////////////////
 
-const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
-    {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
-    {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
-    {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
-    {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
-    {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
-    {name:"Reyna", rating: 3.5, feedback: ""},
+const reviews = [{ name: "Daniela", rating: 5, feedback: "Beautiful atmosphere and wonderful vegan options!" },
+    { name: "Jack", rating: 3, feedback: "A little too hipster for my taste, but the burger was decent, if overpriced" },
+    { name: "Miranda", rating: 4, feedback: "fun trivia and cool vibes" },
+    { name: "Wen", rating: 4.5, feedback: "I don't leave my house often, but when I do, it's for this place. Highly reccomend." },
+    { name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day." },
+    { name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
+    { name: "Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay." },
+    { name: "Reyna", rating: 3.5, feedback: "" },
 ]
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
+reviews[8] = { name: "Julie", rating: 4, feedback: "I love minitacos with extra cheese" };
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays" */
+
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -55,26 +82,34 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 and should return a string in the format `{name} gave the restaurant a {rating}, and their feedback was: {feedback}`
  * 
- * For example, if getReviewByIndex is invoked with reviews and the number 0
+ * For example, if getReviewByIndex is invoked with reviews and the number 0 (0 is the index in the reviews array)
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
-  
+    // return the string by using string interpolation
+    // to access the name we need to use array notation with the index and dot notation for the object property
+    // to access the rating and feedback is same as the name
+    return `${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review and their feedback was: ${reviews[index].feedback}`;
+}
+
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
 getLastReview should accept:
   (1) an array of objects 
   
-and should return a string in the format `name} gave the restaurant a {rating}, and their feedback was: {feedback}`
+and should return a string in the format `{name} gave the restaurant a {rating}, and their feedback was: {feedback}`
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
+
+// function definition with reviews array as a parameter
+function getLastReview(reviews) {
+    // determine the last index in the array
+    let lastIndex = reviews.length - 1;
+    // the last element of an array will have an index one less than the length of the array, i.e. array length of 10 items will have indices 0-9, last review would be index = 9
+    return `${reviews[lastIndex].name} gave the restaurant a ${reviews[lastIndex].rating}, and their feedback was: ${reviews[lastIndex].feedback}`;
+}
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
@@ -91,11 +126,24 @@ function getLastReview(/* code here */) {
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."}]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
-  }
+// ???
+function getReviewByRating(reviews, rating) {
+    // must have an array to store the desired reviews in
+    let theseRatings = [];
+    // for-in loop to check through reviews array
+    for (let index in reviews) {
+        // determine a range around the rating parameter, i'm choosing to return ratings within 0.5 star
+        // i.e. rating = 4.5, then return ratings between 4 and 5 inclusively.
+        if (reviews[index].rating >= rating - 0.5 && reviews[index].rating <= rating + 0.5) {
+            // range is satisfied, use push method to store in theseRatings array
+            theseRatings.push(reviews[index]);
+        }
+    }
+    // for-in loop is complete, we checked each array element and its rating, now return the theseRatings array
+    return theseRatings;
+}
 
-  
+
 /** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words. 
   
 Your function should accept: 
@@ -109,10 +157,10 @@ and should return an array of objects.
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-  function getLongReviews(/* code here */) {
+function getLongReviews( /* code here */ ) {
     /* code here */
-  }
-  
+}
+
 
 /* STRETCH 3:  This challenge is not related to the data above! 
 
@@ -132,7 +180,7 @@ The returned object should have the following characteristics:
 */
 
 
-function carMaker(/* code here */) {
+function carMaker( /* code here */ ) {
     /* code here */
-    
+
 }
